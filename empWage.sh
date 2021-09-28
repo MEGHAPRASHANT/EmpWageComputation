2f40ea9e806c7b7efc1823e9f80a647bbbde5940
MEGHAPRASHANT <meghaprashant92@gmail.com>
2021-09-28 08:20:01 +0400

    Added : daily wage

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,19 @@
-Welcome All
-isPresent=0
-isAbsent=1
-randomNumber=$((RANDOM%2))
-if [ $randomNumber -eq $isPresent ]
-then
-  echo "Employee is present"
-else
-  echo "Employee is absent"
-fi
+echo "welcome all"
+isPresentFullTime=0
+isAbsent=2
+salaryPerHr=20
+day=1
+while [ $day -lt 8 ]
+do
+   randomNumber=$((RANDOM%3))
+   if [ $randomNumber -eq $isPresentFullTime ]
+   then
+     empHr=8
+   else
+     empHr=0
+   fi
+   totalSalary=$((totalSalary+(salaryPerHr*empHr)))
+    ((day++))
+done
+echo $totalSalary
+
